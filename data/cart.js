d97a1668856698393fa6edce80b60f06d6399ad1
Shrinidhi <shrinidhihegde949@gmail.com>
2025-07-17 22:57:55 +0530

    deleting cart items

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
 }, {
@@ -10,7 +10,7 @@ export function addToCart(productId){
     let matchingItem;
 
       cart.forEach((cartItem) => {
-        if (productId === cartItem.producId) {  
+        if (productId === cartItem.productId) {  
           matchingItem = cartItem;
         }
       });
@@ -25,3 +25,15 @@ export function addToCart(productId){
       }
 
 }
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if(cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+};
