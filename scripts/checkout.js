ae718a5d97efb504ccad337ff668b91baa8c6c4c
Shrinidhi <shrinidhihegde949@gmail.com>
2025-07-31 15:10:56 +0530

    removing comments

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,19 +2,13 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts, loadProductsFetch} from '../data/products.js';
 import {loadCart} from '../data/cart.js';
-// import '../data/cart-class.js';
-// import '../data/backend-practice.js';
+
 
 async function loadPage() {
   try {
-    // throw 'error1';
-
     await loadProductsFetch();
-
     const value = await new Promise((resolve, reject) => {
-      // throw 'error2';
       loadCart(() => {
-        // reject('error3');
         resolve('value3');
       });
     });
@@ -27,49 +21,3 @@ async function loadPage() {
   renderPaymentSummary();
 }
 loadPage();
-
-/*
-Promise.all([
-  loadProductsFetch(),
-  new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
-    });
-  })
-
-]).then((values) => {
-  console.log(values);
-  renderOrderSummary();
-  renderPaymentSummary();
-});
-*/
-
-/*
-new Promise((resolve) => {
-  loadProducts(() => {
-    resolve('value1');
-  });
-
-}).then((value) => {
-  console.log(value);
-
-  return new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
-    });
-  });
-
-}).then(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
-});
-*/
-
-/*
-loadProducts(() => {
-  loadCart(() => {
-    renderOrderSummary();
-    renderPaymentSummary();
-  });
-});
-*/
